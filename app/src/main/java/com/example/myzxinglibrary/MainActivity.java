42ff0160d9105a633b82ff8773c09cf2a2b6bcb0
lichao <lichao@163.com>
2020-08-20 11:37:12 +0800

    push

---
+++ app/src/main/java/com/example/myzxinglibrary/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.myzxinglibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.bean.ZxingConfig;
+import com.yzq.zxinglibrary.common.Constant;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                /*ZxingConfig是配置类
+                 *可以设置是否显示底部布局，闪光灯，相册，
+                 * 是否播放提示音  震动
+                 * 设置扫描框颜色等
+                 * 也可以不传这个参数
+                 * */
+                ZxingConfig config = new ZxingConfig();
+                config.setPlayBeep(true);//是否播放扫描声音 默认为true
+                config.setShake(true);//是否震动  默认为true
+                config.setDecodeBarCode(true);//是否扫描条形码 默认为true
+                config.setReactColor(R.color.colorAccent);//设置扫描框四个角的颜色 默认为白色
+                config.setFrameLineColor(R.color.colorAccent);//设置扫描框边框颜色 默认无色
+                config.setScanLineColor(R.color.colorAccent);//设置扫描线的颜色 默认白色
+                config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
+                intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+                startActivityForResult(intent, 11);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 扫描二维码/条码回传
+        if (requestCode == 11 && resultCode == RESULT_OK) {
+            if (data != null) {
+
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                tv.setText("扫描结果为：" + content);
+            }
+        }
+    }
+}
